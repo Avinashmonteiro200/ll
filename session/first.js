bc0fb65cc92b94e7e829298afafbce7c3a43c76c
Avinashmonteiro200 <124524473+Avinashmonteiro200@users.noreply.github.com>
2023-09-24 05:30:42 +0000

    yesover

---
+++ session/first.js
@@ -0,0 +1,33 @@
+// console.log("yes this  u can do it ");
+// let somenumber=33;
+// let gh=String(somenumber);
+// console.log(typeof gh);
+
+//operations
+// let v=3;
+// let neg=-(v);
+// console.log(neg);
+//
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(3/2);
+
+// let str1="hello";
+// let str2=" hi";
+// let str3=str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");//priority it is 
+// console.log(+true);//this plus will give in the result as + as 1
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+// console.log(num1,num2,num3);
+// let gamecounter=100;
+// gamecounter++;
+// console.log(gamecounter);
